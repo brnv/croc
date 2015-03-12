5b7f9f92cb4c5eeb712c1e491e43558319e419cd
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-12 19:32:02 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+//@TODO:
+// table window size
+// 792x546
+// one card size
+// 46x30
+
+//@TODO:
+// 1) parse player chips count
+// 2) parse pot size
+// 3) parse button position
+
+//@TODO:
+// convert -crop command settings
+//
+// hero hand
+// left card - 46x30+346+340
+// right card - 46x30+396+340
+//
