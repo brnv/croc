657ad846ad9d7468b9e4531b9db58b5315241f44
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-15 02:49:22 +0600

    non empty seats parsing

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ func main() {
 }
 
 //@TODO:
-// 1) parse non empty seats, button's position, hero position
-// 1.1) seats: 1 - hero, clockwise - opponents
-// 1.2) positions: dealer, sb, bb, mp2, mp3, cutoff
+// 1) parse button's position, hero position
+// 1.1) positions: dealer, sb, bb, mp2, mp3, cutoff
 // 2) parse current blinds
