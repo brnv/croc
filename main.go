d0ae57d7769cc8baa264c036dffa24188d382413
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-15 01:55:23 +0600

    todos added

--- main.go
+++ main.go
@@ -5,5 +5,7 @@ func main() {
 }
 
 //@TODO:
-// 1) parse button position
+// 1) parse non empty seats, button's position, hero position
+// 1.1) seats: 1 - hero, clockwise - opponents
+// 1.2) positions: dealer, sb, bb, mp2, mp3, cutoff
 // 2) parse current blinds
