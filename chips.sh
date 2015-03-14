1abed59b87f8224ebfa62306c76ccad5baeaf292
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-14 20:31:37 +0600

    chips count recognition

--- chips.sh
+++ chips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function samples_compare {
-	for sample in $(ls ./chips_samples/*); do
+	for sample in $(ls ./pot_samples/*); do
 		diff_percentage=$(compare -quiet -metric RMSE $sample $1 NULL: 2>&1)
 		if [[ $diff_percentage == *"(0)"* ]] || [[ $diff_percentage == *"(0.0"* ]] ; then
 			basename $sample | cut -f1 -d'.' | cat | tr -d '\n'
@@ -19,7 +19,7 @@ function get_tmp_filename {
 
 function recognize {
 	digit=$(get_tmp_filename)
-	convert -crop 9x13+$1+154 $table_image $digit
+	convert -crop 9x13+$1+407 $table_image -colorspace Gray $digit
 	samples_compare $digit
 	rm $digit
 }
@@ -34,14 +34,20 @@ chips_identifier=$(get_tmp_filename)
 
 convert -crop 7x15+396+406 $table_image $chips_identifier
 
-four_digit_chips_similarity=$(compare -quiet -metric RMSE $chips_identifier ./chips_samples/4_digits.png NULL: 2>&1)
+four_digit_chips_similarity=$(compare -quiet -metric RMSE $chips_identifier ./pot_samples/4_digits_chips.png NULL: 2>&1)
 
-three_digit_chips_similarity=$(compare -quiet -metric RMSE $chips_identifier ./chips_samples/3_digits.png NULL: 2>&1)
+three_digit_chips_similarity=$(compare -quiet -metric RMSE $chips_identifier ./pot_samples/3_digits_chips.png NULL: 2>&1)
 
 if [[ $four_digit_chips_similarity == *"(0)"* ]] || [[ $four_digit_chips_similarity == *"(0.0"* ]] ; then
-	echo 4 digit
+	recognize 401
+	recognize 413
+	recognize 422
+	recognize 431
 elif [[ $three_digit_chips_similarity  == *"(0)"* ]] || [[ $three_digit_chips_similarity  == *"(0.0"* ]] ; then
-	echo 3 digit
+	recognize 407
+	recognize 416
+	recognize 425
 else
-	echo 2 digit
+	recognize 412
+	recognize 421
 fi
