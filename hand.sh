b8d12d5cf6a2d2a93e6a14737edb1c706b3310c7
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-15 00:20:27 +0600

    hand parser refactoring

--- hand.sh
+++ hand.sh
@@ -1,50 +1,37 @@
 #!/bin/bash
 
-# refactor
 # implement in go
+
+function samples_compare {
+	for sample in $(ls ./card_samples/*/*); do
+		diff_percentage=$(compare -quiet -metric RMSE $sample $1 NULL: 2>&1)
+		if [[ $diff_percentage == *"(0)"* ]] || [[ $diff_percentage == *"(0.0"* ]] ; then
+			echo $sample | rev | cut -f1-2 -d/ | cut -f2 -d. | tr -d '/' | rev | tr -d '\n'
+		fi
+	done
+}
+
+function uniq_id {
+	echo `date +%N | md5sum | cut -f1 -d' '`
+}
+
+function get_tmp_filename {
+	echo /tmp/croc-$(uniq_id).png
+}
+
+function recognize {
+	card=$(get_tmp_filename)
+	convert -crop 46x30+$1+340 $table_image $card
+	samples_compare $card
+	rm $card
+}
 
 window_id=`xprop | grep -P "window\ id" | grep -o "0x.*"`
 
-uniq_name=/tmp/screenshot_`date +%N | md5sum | cut -f1 -d' '`
+table_image=$(get_tmp_filename)
 
-import -window "$window_id" $uniq_name.png
+import -window "$window_id" $table_image
 
-left_card=$uniq_name.left.png
-
-right_card=$uniq_name.right.png
-
-convert -crop 46x30+346+340 $uniq_name.png $left_card
-
-convert -crop 46x30+396+340 $uniq_name.png $right_card
-
-left_is_uniq="1"
-
-right_is_uniq="1"
-
-for f in $(ls ./card_samples/*/*); do
-	diff_percentage=$(compare -quiet -metric RMSE $left_card $f NULL: 2>&1)
-	if [[ $diff_percentage == *"(0)"* ]]; then
-		left_is_uniq="0"
-		echo $f | rev | cut -f1-2 -d'/' | rev | tr -d / | cut -f1 -d'.'
-	fi
-
-	diff_percentage=$(compare -quiet -metric RMSE $right_card $f NULL: 2>&1)
-	if [[ $diff_percentage == *"(0)"* ]]; then
-		right_is_uniq="0"
-		echo $f | rev | cut -f1-2 -d'/' | rev | tr -d / | cut -f1 -d'.'
-	fi
-done
-
-if [[ $left_is_uniq == "1" ]]; then
-    read -p "enter left card value: " value
-    mkdir -p ./samples/$value
-    read -p "enter left card suit: " suit
-    mv $left_card ./samples/$value/$suit.png
-fi
-
-if [[ $right_is_uniq == "1" ]]; then
-    read -p "enter right card value: " value
-    mkdir -p ./samples/$value
-    read -p "enter right card suit: " suit
-    mv $right_card ./samples/$value/$suit.png
-fi
+recognize 346
+recognize 396
+echo
