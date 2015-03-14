027283d0a3ce681db556c7be55b5a59bafb6f47e
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-14 21:00:00 +0600

    todos edited

--- main.go
+++ main.go
@@ -5,28 +5,6 @@ func main() {
 }
 
 //@TODO:
-// table window size
-// 792x546
-// one card size
-// 46x30
-
-//@TODO:
-// 1) parse player chips count
-// 2) parse pot size
-// 3) parse button position
-// 4) parse board cards
-
-//@TODO:
-// convert -crop command settings
-//
-// hero hand
-// left card - 46x30+346+340
-// right card - 46x30+396+340
-//
-// pot
-// 8x13+390+154 - sample for identification
-// 43x13+390+154 - pot numbers
-//
-// 2 digit pot
-// 9x13+403+154 - first digit
-// 9x13+412+154 - second digit
+// 1) parse button position
+// 2) parse board cards
+// 3) parse current blinds
