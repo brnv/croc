558e0993c4381b1bd14499e485f20bba0c6bf3f5
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-17 18:45:53 +0600

    hand recognition implemented in go

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
+	"sync"
 
 	"github.com/docopt/docopt-go"
 	"github.com/op/go-logging"
@@ -13,10 +16,22 @@ import (
 )
 
 var (
-	log        = logging.MustGetLogger("croc")
-	reWindowId = regexp.MustCompile("window id # of group leader: (0x.*)")
-	cmdRunner  *runcmd.Local
+	log             = logging.MustGetLogger("croc")
+	reWindowId      = regexp.MustCompile("window id # of group leader: (0x.*)")
+	reMatchedSample = regexp.MustCompile("[[:word:]]+/(.*)\\..*$")
+	cmdRunner       *runcmd.Local
+	samplesFiles    map[string][]string
 )
+
+type Table struct {
+	Screenshot string
+
+	HandLeftCardFile  string
+	HandRightCardFile string
+
+	HandFirstCard  string
+	HandSecondCard string
+}
 
 const usage = `
 	Usage:
@@ -32,28 +47,99 @@ func main() {
 		log.Fatal(err)
 	}
 
+	table := Table{}
+
 	args, _ := docopt.Parse(usage, nil, true, "croc", false)
 	if args["<filepath>"] != nil {
-		log.Notice("%v", "File mode")
+		table.Screenshot = args["<filepath>"].(string)
 	} else {
-		log.Notice("%v", "Window mode")
-
-		tableScreenshot, err := makeTableScreenshot()
+		table.Screenshot, err = makeTableScreenshot()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		log.Notice("%v", tableScreenshot)
 	}
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go func() {
+		table.HandFirstCard = recognize(
+			cropFromTableImage(
+				table.Screenshot, 46, 30, 346, 340,
+			), "card_samples/*/*")
+		wg.Done()
+	}()
+
+	go func() {
+		table.HandSecondCard = recognize(
+			cropFromTableImage(
+				table.Screenshot, 46, 30, 396, 340,
+			), "card_samples/*/*")
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	log.Notice("%v", table)
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+	fmt.Printf("Visited: %s\n", path)
+	return nil
+}
+
+func recognize(search string, samplesFilenamePattern string) string {
+	files, _ := filepath.Glob(samplesFilenamePattern)
+
+	for _, file := range files {
+		// try -metric AE
+		// adjuct -fuzz option
+		// speed up with goroutines
+		command, _ := cmdRunner.Command(fmt.Sprintf(
+			"/bin/compare -quiet -metric RMSE -fuzz 0 %s %s NULL:",
+			file, search))
+
+		_, err := command.Run()
+		if err == nil {
+			matches := reMatchedSample.FindStringSubmatch(file)
+
+			if len(matches) != 0 {
+				return strings.Replace(matches[1], "/", "", 1)
+			}
+		}
+	}
+
+	return search
+}
+
+func cropFromTableImage(
+	table string,
+	cropWidth int,
+	cropHeight int,
+	offsetX int,
+	offsetY int,
+) string {
+	cropped, _ := getTmpFilename()
+
+	command, _ := cmdRunner.Command(fmt.Sprintf(
+		"/bin/convert -crop %dx%d+%d+%d %s %s",
+		cropWidth, cropHeight, offsetX, offsetY,
+		table, cropped),
+	)
+
+	_, _ = command.Run()
+
+	return cropped
 }
 
 func makeTableScreenshot() (string, error) {
-	tableScreenshot, err := getTmpFilename("table")
+	tableScreenshot, err := getTmpFilename()
 	if err != nil {
 		return "", err
 	}
 
-	c, err := cmdRunner.Command(fmt.Sprintf(
+	command, err := cmdRunner.Command(fmt.Sprintf(
 		"/bin/import png:%s", tableScreenshot),
 	)
 
@@ -61,7 +147,7 @@ func makeTableScreenshot() (string, error) {
 		return "", err
 	}
 
-	_, err = c.Run()
+	_, err = command.Run()
 	if err != nil {
 		return "", err
 	}
@@ -70,9 +156,9 @@ func makeTableScreenshot() (string, error) {
 }
 
 func getWindowId() (string, error) {
-	c, err := cmdRunner.Command("/bin/xprop")
+	command, err := cmdRunner.Command("/bin/xprop")
 
-	output, err := c.Run()
+	output, err := command.Run()
 	if err != nil {
 		return "", err
 	}
@@ -86,8 +172,8 @@ func getWindowId() (string, error) {
 	return "", errors.New("No window id found")
 }
 
-func getTmpFilename(postfix string) (string, error) {
-	file, err := ioutil.TempFile(os.TempDir(), "croc-"+postfix)
+func getTmpFilename() (string, error) {
+	file, err := ioutil.TempFile(os.TempDir(), "croc")
 	if err != nil {
 		return "", err
 	}
