1a11f40835be291a25147d8446c27507ba45aced
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-19 16:50:41 +0600

    hand recognition moved to hand.go

--- main.go
+++ main.go
@@ -81,36 +81,7 @@ func main() {
 		}
 	}
 
-	hand := Hand{
-		LeftCard: Card{
-			ImageSnippet: ImageSnippet{
-				Width:   cardWidth,
-				Height:  cardHeight,
-				OffsetX: handLeftCardOffsetX,
-				OffsetY: handCardOffsetY,
-			},
-		},
-		RightCard: Card{
-			ImageSnippet: ImageSnippet{
-				Width:   cardWidth,
-				Height:  cardHeight,
-				OffsetX: handRightCardOffsetX,
-				OffsetY: handCardOffsetY,
-			},
-		},
-	}
-
-	leftCard, err := recognize(image.Crop(hand.LeftCard.ImageSnippet), cardSamples)
-	if err != nil {
-		log.Notice("%v", err.Error())
-	}
-	log.Notice("%v\n", leftCard)
-
-	rightCard, err := recognize(image.Crop(hand.RightCard.ImageSnippet), cardSamples)
-	if err != nil {
-		log.Notice("%v", err.Error())
-	}
-	log.Notice("%v\n", rightCard)
+	image.HandRecognize()
 }
 
 func recognize(
