362ee064e1eb4dca742cb5682a20e2a2fbd00cbb
Alexey Baranov <a.baranov@office.ngs.ru>
2015-03-20 19:25:10 +0600

    cleaning up

--- screens.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-window_id=`xprop | grep -P "window\ id\ #\ of" | grep -o "0x.*"`
-
-while true; do
-	uniq_name=/tmp/screenshot_`date +%N | md5sum | cut -f1 -d' '`
-	import -window "$window_id" $uniq_name.png
-	sleep 1s
-done
