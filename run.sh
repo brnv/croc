b8e061f41adb5cfcb92a043874bfbcd2ad085204
Alexey Baranov <baranov.alxy@gmail.com>
2015-09-11 23:08:17 +0600

    wasted meter

--- run.sh
+++ run.sh
@@ -3,29 +3,41 @@
 window_id=`xwininfo | grep -Po "Window\sid:\s([\S.]+)\s" | cut -f3 -d' '`
 
 if [[ -z $window_id ]]; then
-	exit
+    exit
 fi
 
 hand_num=1
 
+no_action_times=0
+
 while true; do
-	echo -n \#
+    echo -n \#
 
-	start=`date +%s%N`
-	result=`./croc --wid $window_id -v $1`
-	code=$?
-	stop=`date +%s%N`
+    start=`date +%s%N`
+    result=`./croc --wid $window_id -v $1`
+    code=$?
+    stop=`date +%s%N`
 
-	if [[ $code == 0 ]]; then
-		echo $hand_num $(($stop - $start)) $result
+    if [[ $code == 0 ]]; then
+        echo $hand_num $(($stop - $start)) $result
 
-		hand_num=$(($hand_num+1))
-	fi
+        hand_num=$(($hand_num+1))
+    fi
 
-	echo $result | grep -Poq MANUAL
-	if [[ $? == 0 ]]; then
-		 espeak "Attention!"
-	fi
+    if [[ -z $result ]]; then
+        no_action_times=$(($no_action_times+1))
+    else
+        no_action_times=0
+    fi
 
-	sleep 2s
+    if [[ $no_action_times -ge 15 ]]; then
+        espeak "Wasted!"
+    fi
+
+    echo $result | grep -Poq MANUAL
+    if [[ $? == 0 ]]; then
+        espeak "Attention!"
+    fi
+
+    sleep 2s
 done
